d41460b7f4f50e1af63f47469bce745509971324
tom <digiton.pl@gmail.com>
2024-10-24 22:34:05 +0200

    make use of records

--- src/main/java/sb/Team.java
+++ src/main/java/sb/Team.java
@@ -1,30 +1,3 @@
 package sb;
 
-import java.util.Objects;
-
-public class Team {
-    private final String name;
-
-    public Team(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Team)) return false;
-        Team team = (Team) o;
-        return Objects.equals(name, team.name);
-    }
-
-    @Override
-    public String toString() {
-        return "Team{" +
-                "name='" + name + '\'' +
-                '}';
-    }
-}
+public record Team(String name) {}
